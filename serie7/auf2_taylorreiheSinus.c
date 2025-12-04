542d3a04825fc9eb2a104c4b4156d9ff982516d2
Blue <pfefferkorn304@web.de>
2025-12-04 09:59:59 +0100

    Serie 7 Aufgabe 2: Taylor-Reihe wird korrekt bestimmt, Programm läuft, und gibt korrekte Ergebnisse aus

--- serie7/auf2_taylorreiheSinus.c
+++ serie7/auf2_taylorreiheSinus.c
@@ -3,29 +3,37 @@
 #include <math.h>
 
 // Funktion zur Berechnung einer Fakultät
-int fact(int n)
+double fact(int n)
 {
   if (n==1)
     return 1;
   else
+    //printf("@fact: n %d\n",n);
     return fact(n-1)*n;
 }
 
 // Funktion für die Taylor-Reihe
-double taylorSinus(int (*fact)(int), int stop, double x) 
+double taylorSinus(int stop, double x) 
 {
 // Funktion: 
 // sin(x) = sum_0^inf (-1)^n * ( x^(2n+1) ) / ((2n+1)!)
 
-  int sign = -1; 
+  int sign = 1; 
     // für wechselndes Vorzeichen
   int n = 1;
   
-  double taylorSin;
+  double taylorSin = 0;
   
-  for (taylorSin = 0; n <= stop; n+=2, sign=sign*-1)
-    taylorSin = taylorSin + sign * (x,n)/fact(n);
-  
+  for (taylorSin = 0; n <= stop; n+=2)
+  {
+    double fac = fact(n);
+    //printf("fac1: %lf", fac);
+    taylorSin = taylorSin + sign * pow(x,n)/fac;
+    sign = sign * (-1);
+    //printf("\n\ntaylorSin %.4lf, sign %d, x %.1lf, n %d\n",taylorSin, sign, x, n);
+    //printf("pow(x,n) %lf, fac(n) %lf\n\n", pow(x,n),fact(n));
+  }
+
   return taylorSin;
 }
 
@@ -42,15 +50,16 @@ int main(void)
   double step = 0.5;
 
   printf("Geben Sie die maximale Potenz der Tayler-Reihe an: ");
-  scanf("%d\n", &maxPot);
+  scanf("%d", &maxPot);
 
-  while (x >= 0 && x <= 10)
+  printf("\n x\t  sin(x) \t  taylor(x)\n");
+
+  while (x >= 0 && x <= 10) // TODO set x <= 10
   {
     mathSin = sin(x);
-    taylorSin = taylorSinus(fact,maxPot,x);
+    taylorSin = taylorSinus(maxPot,x);
 
-    printf("\nx\tsin(x)\ttaylor(x)\n");
-    printf("%.1lf %.4lf %.4lf", x, mathSin,taylorSin);
+    printf("%.1lf\t%7.4lf\t\t%9.4lf\n", x, mathSin,taylorSin);
     x = x + step;
   }
 
