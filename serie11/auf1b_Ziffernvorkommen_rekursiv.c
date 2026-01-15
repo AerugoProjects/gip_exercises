a9b07c3330a342687321f09e42b099e55d74a463
Blue <pfefferkorn304@web.de>
2026-01-15 20:30:11 +0100

    Serie 11 Aufgabe 1b: rekursive Bestimmung des Vorkommens einer Ziffer in einer Zahl

---
+++ serie11/auf1b_Ziffernvorkommen_rekursiv.c
@@ -0,0 +1,51 @@
+/*
+Geben Sie für die folgenden Probleme jeweils einen rekursiven Algorithmus in Form einer C-Funktion an:
+
+b) Für eine natürliche Zahl n → 0 soll die Anzahl der Vor-
+kommen einer Ziffer bestimmt werden. Sowohl die Zahl n als 
+auch die Ziffer sollen der Funktion dabei als Parameter über-
+geben werden.
+*/
+
+#include <stdio.h>
+
+int funcCountDigit (int number, int digit)
+{
+  if (number < digit)
+    // Zahl ist kleiner als gesuchte Ziffer
+    return 0;
+  if (number == digit)
+    // Zahl ist gleich der gesuchten Ziffer
+    return 1;
+  if (number/10 > 0)
+  {
+    if (number % 10 == digit)
+      return ( 1 + funcCountDigit(number/10,digit) );
+    else
+      return funcCountDigit(number/10,digit);
+  }
+}
+
+
+int main(void)
+{
+  int number = 0;
+    // Zahl
+  int digit = 0;
+    // gesuchte Ziffer
+  
+  printf("Geben sie Zahl ein, die nach einer Ziffer durchsucht");
+  printf(" werden soll: ");
+  scanf("%d", &number);
+    // Zahl von Benutzer abfragen
+  
+  printf("Geben sie die zu zählende Ziffer ein: ");
+  scanf("%1d", &digit);
+    // Ziffer von Benutzer abfragen
+
+  printf("Die gesuchte Ziffer %d kommt %d mal in %d vor.\n", 
+    digit, funcCountDigit(number, digit), number);
+    // Ausgabe des Ergebnis
+  
+  return 0;
+}
