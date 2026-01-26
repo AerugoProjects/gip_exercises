d7ea08dd769b11e36e1f87616e9e3b0ec8adbf34
Blue <pfefferkorn304@web.de>
2026-01-26 09:43:16 +0100

    Serie 13: Haensel und Gretel Deluxe der belgischen Firma Smart GAmes NV

---
+++ serie13/HaenselUndGretel_v1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// // //
+// 
+// structs
+// 
+// // //
+
+
+
+// // //
+// 
+// Funktionen
+// 
+// // //
+
+
+
+// // //
+// 
+// main
+// 
+// // //
+
+int main (void)
+{
+  return 0;
+}
